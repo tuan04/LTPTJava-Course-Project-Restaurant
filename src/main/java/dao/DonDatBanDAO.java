11066502e722c08dca40090b4c0e3b139def7169
Thanh <ttung2827@gmail.com>
2025-01-21 19:12:54 +0700

    update dao LoaiKhachHanh KhachHang DonDatBan

--- src/main/java/dao/DonDatBanDAO.java
+++ src/main/java/dao/DonDatBanDAO.java
@@ -1,4 +1,53 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import model.DonDatBan;
+
+import java.util.List;
+
+
 public class DonDatBanDAO {
+
+    private static EntityManager em = Persistence.createEntityManagerFactory(
+            "mariadb-pu"
+    ).createEntityManager();
+
+    public static boolean creat(DonDatBan donDatBan){
+        EntityTransaction tr = em.getTransaction();
+        try {
+            tr.begin();
+            em.persist(donDatBan);
+            tr.commit();
+            return  true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            tr.rollback();
+        }
+        return false;
+    }
+
+    public static boolean update(DonDatBan donDatBan){
+        EntityTransaction tr = em.getTransaction();
+        try{
+            tr.begin();
+            em.merge(donDatBan);
+            tr.commit();
+            return true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            tr.rollback();
+        }
+        return false;
+    }
+
+    public static List<DonDatBan> getAll(){
+        String query = "select don from DonDatBan don";
+        return em.createQuery(query,DonDatBan.class).getResultList();
+    }
+
+    public static DonDatBan findById(String maDon){
+        return em.find(DonDatBan.class, maDon);
+    }
 }
