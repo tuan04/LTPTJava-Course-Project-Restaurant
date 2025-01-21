dfc70b4561eaacae71abd6d6e22c1add76322945
Tâm Thành <tamthanh@MacBook-Air-cua-Tam-2.local>
2025-01-21 20:35:45 +0700

    update LoaiMonAnDAO

--- src/main/java/dao/LoaiMonAnDAO.java
+++ src/main/java/dao/LoaiMonAnDAO.java
@@ -1,5 +1,12 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import model.LoaiMonAn;
+
+import java.util.List;
+
 public class LoaiMonAnDAO {
     private EntityManager em = Persistence.createEntityManagerFactory("mariadb-pu")
             .createEntityManager();
