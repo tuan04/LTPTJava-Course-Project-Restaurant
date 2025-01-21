056f1d39e9738f0b0336bb4e0ffe6dd1bd0bc06f
Tâm Thành <tamthanh@MacBook-Air-cua-Tam-2.local>
2025-01-21 20:33:32 +0700

    update LoaiMonAnDAO

--- src/main/java/dao/LoaiMonAnDAO.java
+++ src/main/java/dao/LoaiMonAnDAO.java
@@ -1,4 +1,32 @@
 package dao;
 
 public class LoaiMonAnDAO {
+    private EntityManager em = Persistence.createEntityManagerFactory("mariadb-pu")
+            .createEntityManager();
+
+    public boolean updateLoaiMonAn(LoaiMonAn loaimonan){
+        EntityTransaction tr = em.getTransaction();
+        try{
+            tr.begin();
+            em.merge(loaimonan);
+            tr.commit();
+            return true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            tr.rollback();
+        }
+        return false;
+    }
+
+    //
+    public static List<LoaiMonAn> getAll(){
+        String query = "select * from LoaiMonAn;
+        return em.createQuery(query, LoaiMonAn.class).getResultList();
+    }
+
+    public static LoaiMonAn findByID(String maLoaiMon){
+        return em.find(LoaiMonAn.class, maLoaiMon;
+    }
+
+
 }
