95d9684ec006d7542b5a445934ea668efd9915b4
Khổng Tám <22690521.tam@student.iuh.edu.vn>
2025-01-21 19:27:50 +0700

    update HoaDonDAO

--- src/main/java/dao/HoaDonDAO.java
+++ src/main/java/dao/HoaDonDAO.java
@@ -1,4 +1,43 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.Persistence;
+import model.HoaDon;
+
 public class HoaDonDAO {
+    private EntityManager em = Persistence.createEntityManagerFactory("mariadb-pu")
+            .createEntityManager();;
+
+    public HoaDon getHoaDon(int maHD) {
+        return em.find(HoaDon.class, maHD);
+    }
+    public boolean createrOrder(HoaDon hd){
+        try {
+            em.getTransaction().begin();
+            em.persist(hd);
+            em.getTransaction().commit();
+            return true;
+        }catch (Exception e){
+
+            em.getTransaction().rollback();
+        }
+        return false;
+
+    }
+    public boolean updateOrder(HoaDon hd){
+        try {
+            em.getTransaction().begin();
+            em.merge(hd);
+            em.getTransaction().commit();
+
+            return true;
+
+
+        }catch (Exception e){
+
+            em.getTransaction().rollback();
+        }
+        return false;
+    }
+
 }
