2376de89ed825c7f00897418b3612979681fcb17
Tâm Thành <tamthanh@MacBook-Air-cua-Tam-2.local>
2025-01-21 20:21:32 +0700

    update LoaiKhuyenMaiDAO

--- src/main/java/dao/LoaiKhuyenMaiDAO.java
+++ src/main/java/dao/LoaiKhuyenMaiDAO.java
@@ -1,4 +1,39 @@
 package dao;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityTransaction;
+import jakarta.persistence.Persistence;
+import model.LoaiKhuyenMai;
+
+import java.util.List;
+
 public class LoaiKhuyenMaiDAO {
+
+    private EntityManager em = Persistence.createEntityManagerFactory("mariadb-pu")
+            .createEntityManager();
+
+    public boolean updateLoaiKhuyenMai(LoaiKhuyenMai loaikhuyenmai){
+        EntityTransaction tr = em.getTransaction();
+        try{
+            tr.begin();
+            em.merge(loaikhuyenmai);
+            tr.commit();
+            return true;
+        }catch (Exception ex){
+            ex.printStackTrace();
+            tr.rollback();
+        }
+        return false;
+    }
+
+    //
+    public static List<LoaiKhuyenMai> getAll(){
+        String query = "select * from LoaiKhuyenMai;
+        return em.createQuery(query, LoaiKhuyenMai.class).getResultList();
+    }
+
+    public static LoaiKhuyenMai findByID(String maLoaiKM){
+        return em.find(LoaiKhuyenMai.class, maLoaiKM);
+    }
+
 }
